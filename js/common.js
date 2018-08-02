988600c61e2570d1150b82815f2b7a93aa1d8c18
Psiu <psiu7217@gmail.com>
2018-08-02 11:59:02 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,54 @@
+$(document).ready ( function(){
+
+
+    // начать повторы с интервалом 2 сек
+    var id_slide = 1;
+    if ($(window).width() >= 720 ) {
+
+        var timerId = setInterval(function() {
+            id_slide++;
+            if (id_slide == 5) {
+                id_slide = 1;
+            }
+
+            $('.section_4 .descr .slide_v1').hide();
+            $('.section_4 .descr .slide_v1[data-slide = '+id_slide+']').show();
+
+            $('.section_4 .navigate .btn').removeClass('active');
+            $('.section_4 .navigate .btn[data-slide ='+id_slide+']').addClass('active');
+        }, 5000);
+
+    }else {
+        $('.section_4 .descr p.btn').click(function () {
+            var id = $(this).data('slide');
+            $('.section_4 .descr p.btn').removeClass('active');
+            $(this).addClass('active');
+            $('.section_4 .descr .slide_v1').hide();
+            $('.section_4 .descr .slide_v1[data-slide = '+id+']').show();
+        });
+    }
+
+
+    //Банер на 4 слайде
+    $('.section_4 .navigate .btn').click(function () {
+        var id = $(this).data('slide');
+        clearInterval(timerId);//Остановка автоматического слайда
+        $('.section_4 .navigate .btn').removeClass('active');
+        $(this).addClass('active');
+
+        $('.section_4 .descr .slide_v1').hide();
+        $('.section_4 .descr .slide_v1[data-slide = '+id+']').show();
+    });
+
+    var h = $('.section.section_14').css('height');
+    $('.section.section_14 iframe').css('height', h );
+
+
+    //Модальное окно
+    $('button[data-toggle = modal]').click(function () {
+        $('#exampleModal').show();
+    });
+    $('.modal .close, .modal .bg, .modal .btn.btn-secondary').click(function () {
+        $('.modal').hide();
+    });
+});
---
+++ mail.php
@@ -0,0 +1,30 @@
+<?php
+if ($_POST['phone'] == '') {
+
+    $to = 'sale@webernetic.by';
+    //$to = 'dev@webernetic.by';
+
+    $subject = 'Заявка с SEO лендинга';
+
+    $headers = "From: webernetic.by\r\n";
+    $headers .= "CC: info@webernetic.by\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    $message = '<html><body>';
+    $message .= '<h1 style="color:#000;">Заявка с лендинга seo.webernetic.by</h1>';
+    $message .= '<p><strong>Имя:</strong> '. $_POST['user_name'] .'</p>';
+    $message .= '<p><strong>Email:</strong> '. $_POST['user_mail'] .'</p>';
+    $message .= '<p><strong>Телефон:</strong> '. $_POST['user_phone'] .'</p>';
+    $message .= '<p><strong>Адрес сайта:</strong> '. $_POST['user_site_url'] .'</p>';
+    $message .= '</body></html>';
+
+
+    mail($to, $subject, $message, $headers);
+    //Это для Ани маркетолога
+    mail('dao@webernetic.by', $subject, $message, $headers);
+
+
+}else {
+    echo 'bot';
+}
